98994ffeaff06adcd5e056509118cdc1ecba6f39
MyDesktop\VIEGo <vigothirapat2548@gmail.com>
2026-03-09 17:19:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
-const INCLUDE_DIR = __DIR__ .  '/../includes';
-const ROUTE_DIR = __DIR__ . '/../routes';
-const TEMPLATES_DIR = __DIR__ . '/../templates';
-const DATABASES_DIR = __DIR__ . '/../databases';
+const INCLUDE_DIR = __DIR__ .  '/includes';
+const ROUTE_DIR = __DIR__ . '/routes';
+const TEMPLATES_DIR = __DIR__ . '/templates';
+const DATABASES_DIR = __DIR__ . '/databases';
 
 require_once INCLUDE_DIR . '/database.php';
 require_once INCLUDE_DIR . '/otp.php';
